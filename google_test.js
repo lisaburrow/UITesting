6d7c62797c0fbe5b617886948e35e3d0f6dd65c3
Lisa Burrow <lisa.burrow@johnlewis.co.uk>
2020-05-18 17:56:26 +0100

    Made Browser be Chrome

--- google_test.js
+++ google_test.js
@@ -3,21 +3,24 @@ const webdriver = require('selenium-webdriver'),
     until = webdriver.until;
 
     const driver = new webdriver.Builder()
-    .forBrowser('firefox')
+    //.forBrowser('firefox')
+    .forBrowser('chrome')
     .build();
+
 
 driver.get('http://www.google.com');
 
 driver.findElement(By.name('q')).sendKeys('webdriver');
 
-driver.sleep(1000).then(function() {
+driver.sleep(10000).then(function() {
   driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
 });
 
 driver.findElement(By.name('btnK')).click();
 
-driver.sleep(2000).then(function() {
+driver.sleep(10000).then(function() {
   driver.getTitle().then(function(title) {
+    console.log(title);
     if(title === 'webdriver - Google Search') {
       console.log('Test passed');
     } else {
